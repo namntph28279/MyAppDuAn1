ea9617a138043ffa0089810bcffb26938838bff8
namntph28279 <namntph28279@fpt.edu.vn>
2022-11-21 12:26:16 +0700

    abc

--- app/src/main/java/com/example/myapp/Activity/MainActivity.java
+++ app/src/main/java/com/example/myapp/Activity/MainActivity.java
@@ -1,8 +1,10 @@
-package com.example.myapp;
+package com.example.myapp.Activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.example.myapp.R;
 
 public class MainActivity extends AppCompatActivity {
 
