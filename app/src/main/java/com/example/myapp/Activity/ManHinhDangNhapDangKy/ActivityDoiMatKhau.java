a996950dd5622d5e680b4e52a15583b60243565f
namntph28279 <namntph28279@fpt.edu.vn>
2022-11-22 20:31:53 +0700

    abc

---
+++ app/src/main/java/com/example/myapp/Activity/ManHinhDangNhapDangKy/ActivityDoiMatKhau.java
@@ -0,0 +1,16 @@
+package com.example.myapp.Activity.ManHinhDangNhapDangKy;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.myapp.R;
+
+public class ActivityDoiMatKhau extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_doi_mat_khau);
+    }
+}
