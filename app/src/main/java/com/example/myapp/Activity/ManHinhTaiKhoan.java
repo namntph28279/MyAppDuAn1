687c78df9d52e3582c4a53b7e89b58c1c718feb3
haivvph24197 <haivvph24197@fpt.edu.vn>
2022-11-22 10:00:58 +0700

    hải

---
+++ app/src/main/java/com/example/myapp/Activity/ManHinhTaiKhoan.java
@@ -0,0 +1,16 @@
+package com.example.myapp.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.myapp.R;
+
+public class ManHinhTaiKhoan extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_man_hinh_tai_khoan);
+    }
+}
